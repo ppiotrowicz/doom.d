916dc157dd93f2c454a9fbd1a3c3dfa688dc385a
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2019-12-25 23:13:51 +0100

    Align bindings

--- +bindings.el
+++ +bindings.el
@@ -12,15 +12,15 @@
    :n "C-l"    #'evil-window-right)
 
  (:leader
-   :desc "vterm here"   :n "!" #'+vterm/toggle
-   :desc "neotree find" :n "t" #'+neotree/find-this-file
-   :desc "neotree open" :n "T" #'+neotree/open
-   :desc "search"       :n "/" #'+default/search-project
+   :desc "vterm here"       :n "!" #'+vterm/toggle
+   :desc "neotree find"     :n "t" #'+neotree/find-this-file
+   :desc "neotree open"     :n "T" #'+neotree/open
+   :desc "search"           :n "/" #'+default/search-project
 
    (:desc "buffers" :prefix "b"
-     :desc "switch"          :n "b" #'+ivy/switch-buffer
-     :desc "kill buffer"     :n "d" #'kill-this-buffer
-     :desc "new buffer"      :n "n" #'evil-buffer-new)
+     :desc "switch"         :n "b" #'+ivy/switch-buffer
+     :desc "kill buffer"    :n "d" #'kill-this-buffer
+     :desc "new buffer"     :n "n" #'evil-buffer-new)
 
    (:desc "project" :prefix "p"
      :desc "switch project" :n "p" #'counsel-projectile-switch-project
@@ -32,10 +32,10 @@
      )
 
    (:desc "windows" :prefix "w"
-     :desc "split -"    :n "s" #'evil-window-split
-     :desc "split |"    :n "v" #'evil-window-vsplit
-     :desc "close"      :n "c" #'+workspace/close-window-or-workspace
-     :desc "balance"    :n "=" #'balance-windows
-     :desc "ace window" :n "w" #'ace-window
-     :desc "fullscreen" :n "f" #'pp/toggle-fullscreen))
+     :desc "split -"        :n "s" #'evil-window-split
+     :desc "split |"        :n "v" #'evil-window-vsplit
+     :desc "close"          :n "c" #'+workspace/close-window-or-workspace
+     :desc "balance"        :n "=" #'balance-windows
+     :desc "ace window"     :n "w" #'ace-window
+     :desc "fullscreen"     :n "f" #'pp/toggle-fullscreen))
  )
