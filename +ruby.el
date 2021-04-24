63ae93d267540971c672a63e4769c90713f8251a
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2021-04-24 13:16:43 +0200

    RSpec docker

--- +ruby.el
+++ +ruby.el
@@ -9,8 +9,6 @@
   (interactive)
   (kill-buffer "*rspec-compilation*"))
 
-(setq rspec-use-spring-when-possible t)
-
 (setq flycheck-command-wrapper-function
       (lambda (command)
         (if (string-suffix-p "rubocop" (car command))
@@ -20,3 +18,27 @@
 (after! flycheck
   (setq-default flycheck-disabled-checkers '(ruby-reek))
 )
+
+;; Leadsimple settings
+(setq rspec-use-docker-when-possible t)
+(setq rspec-docker-command "docker-compose run --rm")
+(setq rspec-docker-cwd "/www/components/core/")
+(setq rspec-docker-file-name "../../docker-compose.yml")
+(setq rspec-docker-container "web")
+
+
+
+(add-hook! rspec-mode :append (defun pp/override-rspec-functions ()
+                            "Overrides rspec docker functions"
+                            (defun rspec--docker-default-wrapper (rspec-docker-command rspec-docker-container rspec-docker-cwd command)
+                              "Function for wrapping a command for execution inside a dockerized environment. "
+                              (format "%s %s sh -c \"cd %s &&  %s\"" rspec-docker-command rspec-docker-container rspec-docker-cwd command))
+
+                            (defun rspec--docker-wrapper (command)
+                              (if (rspec-docker-p)
+                                  (funcall rspec-docker-wrapper-fn
+                                           rspec-docker-command
+                                           rspec-docker-container
+                                           rspec-docker-cwd
+                                           command)
+                                command))))
